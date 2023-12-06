8d48a013d7badb2e0f9e0c923d4217a5b841e657
Viacheslav Leshchenko <slaval.125.sl@gmail.com>
2023-12-06 22:52:19 +0300

    Added more projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,30 @@ class DetailsBtn
   }
 }
 
-document.querySelectorAll('.skill__title').forEach((el) => {
+document.querySelectorAll('button').forEach((el) => {
   new DetailsBtn(el);
+});
+
+var main_image = document.querySelector(".project-gallery-main-item");
+
+main_image.addEventListener('click', (e) => {
+  main_image.removeAttribute("visible");
+});
+
+class GalleryBtn
+{
+  constructor(el) {
+    this.el = el;
+
+    this.el.addEventListener('click', (e) => this.onClick(e));
+  }
+
+  onClick(e) {
+    main_image.children[0].src = this.el.children[0].src;
+    main_image.setAttribute("visible", "");
+  }
+}
+
+document.querySelectorAll('.project-gallery-item').forEach((el) => {
+  new GalleryBtn(el);
 });
