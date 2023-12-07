c946cb82651cb6e76989c9c209cc984badd432e9
Viacheslav Leshchenko <slaval.125.sl@gmail.com>
2023-12-08 00:31:56 +0300

    almost final version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-// Открытие/закрытие меню при клике на кнопку "menuSwitcherButton"
 const menuSwitcherButton = document.querySelector('.menu-switcher');
 const navigation = document.querySelector('.header__navigation');
 menuSwitcherButton.addEventListener('click', (e) => {
@@ -6,14 +5,12 @@ menuSwitcherButton.addEventListener('click', (e) => {
     menuSwitcherButton.classList.toggle('menu-switcher_opened');
 });
 
-// Закрытие меню при клике на любую из ссылок в меню
 navigation.querySelectorAll('.navigation__link').forEach(link => {
     link.addEventListener('click', e => {
         navigation.classList.toggle('header__navigation_opened');
         menuSwitcherButton.classList.toggle('menu-switcher_opened');
     })
 })
-
 
 class DetailsBtn
 {
